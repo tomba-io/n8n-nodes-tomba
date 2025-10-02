278664266e76e55eba29ba342c6623254da8bd4c
Mohamed Ben <b.mohamed@tomba.io>
2025-10-02 15:00:41 +0100

    fix: refactor tombaApiRequest for improved type safety and clarity

--- nodes/Tomba/GenericFunctions.ts
+++ nodes/Tomba/GenericFunctions.ts
@@ -1,27 +1,29 @@
-import { OptionsWithUri } from 'request';
-import {
+import type {
+	IDataObject,
 	IExecuteFunctions,
-	IExecuteSingleFunctions,
 	IHookFunctions,
+	IHttpRequestMethods,
 	ILoadOptionsFunctions,
-} from 'n8n-core';
-import { IDataObject, NodeApiError, NodeOperationError } from 'n8n-workflow';
+	IHttpRequestOptions,
+	JsonObject,
+} from 'n8n-workflow';
+import { NodeApiError, NodeOperationError } from 'n8n-workflow';
 
 export async function tombaApiRequest(
-	this: IHookFunctions | IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions,
-	method: string,
+	this: IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions,
+	method: IHttpRequestMethods,
 	resource: string,
+
 	body: any = {},
 	qs: IDataObject = {},
 	uri?: string,
 	option: IDataObject = {},
 ): Promise<any> {
-	// tslint:disable-line:no-any
 	const credentials = await this.getCredentials('tombaApi');
 	if (credentials === undefined) {
 		throw new NodeOperationError(this.getNode(), 'No credentials got returned!');
 	}
-	let options: OptionsWithUri = {
+	let options: IHttpRequestOptions = {
 		headers: {
 			Accept: 'application/json',
 			'Content-Type': 'application.json',
@@ -31,16 +33,42 @@ export async function tombaApiRequest(
 		method,
 		qs,
 		body,
-		uri: uri || `https://api.tomba.io/v1${resource}`,
+		url: uri || `https://api.tomba.io/v1${resource}`,
 		json: true,
 	};
 	options = Object.assign({}, options, option);
-	if (Object.keys(options.body).length === 0) {
+	if (Object.keys(options.body as IDataObject).length === 0) {
 		delete options.body;
 	}
 	try {
-		return await this.helpers.request!(options);
+		return await this.helpers.request(options);
 	} catch (error) {
-		throw new NodeApiError(this.getNode(), error);
+		throw new NodeApiError(this.getNode(), error as JsonObject);
 	}
+}
+
+export async function tombaApiRequestAllItems(
+	this: IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions,
+	propertyName: string,
+	method: IHttpRequestMethods,
+	resource: string,
+
+	body: any = {},
+	query: IDataObject = {},
+): Promise<any> {
+	const returnData: IDataObject[] = [];
+
+	let responseData;
+	query.page = 0;
+	query.limit = 100;
+
+	do {
+		responseData = await tombaApiRequest.call(this, method, resource, body, query);
+		returnData.push(responseData[propertyName] as IDataObject);
+		query.page += query.limit;
+	} while (
+		responseData.meta?.total !== undefined &&
+		responseData.meta.page <= responseData.meta.total
+	);
+	return returnData;
 }
