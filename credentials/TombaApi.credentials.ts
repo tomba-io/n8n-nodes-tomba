b2b093e2f280cfb365668b797b8faae35d86a74e
Mohamed Ben <b.mohamed@tomba.io>
2025-10-02 14:59:02 +0100

    fix: update Tomba API credentials with improved documentation and properties

--- credentials/TombaApi.credentials.ts
+++ credentials/TombaApi.credentials.ts
@@ -3,20 +3,27 @@ import { ICredentialType, INodeProperties } from 'n8n-workflow';
 export class TombaApi implements ICredentialType {
 	name = 'tombaApi';
 	displayName = 'Tomba API';
-	documentationUrl = 'https://developer.tomba.io/';
+	documentationUrl = 'https://app.tomba.io/api';
 	properties: INodeProperties[] = [
 		{
 			displayName: 'API Key',
 			name: 'apiKey',
 			type: 'string',
-			typeOptions: { password: true },
+			typeOptions: { password: true, minValue: 30 },
 			default: '',
+			placeholder: 'ta_xx',
+			description: 'Your Tomba API Key',
+			hint: 'You can find your API key in your Tomba account API page.',
 		},
 		{
 			displayName: 'API Secret',
 			name: 'secretKey',
 			type: 'string',
+			typeOptions: { password: true, minValue: 30 },
 			default: '',
+			placeholder: 'ts_xx',
+			description: 'Your Tomba API Secret',
+			hint: 'You can find your API secret in your Tomba account API page.',
 		},
 	];
 }
